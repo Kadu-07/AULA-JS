5c61b98cb4ad98b7e37b88f13fa07c35d0584b1d
Carlos <cadubalbueno2007@gmail.com>
2026-05-12 10:11:43 -0300

    Esqueci o tipos_dados

--- AULA01/tipos_dados.js
+++ AULA01/tipos_dados.js
@@ -6,12 +6,25 @@ console.log(`Ermeson Barros`);
 console.log(`Nota do aluno foi: ` + 8);
 console.log(6 >= 7);
 
-// const
-// let
-// var
+// const | Não pode ser redeclarado e não ter seu valor redefinido.
+// let | Não pode ser redeclarado, porém seu valor pode ser redefinido.
+// var | Pode ser redeclardo e pode ter seu valor alterado.
 
-const CPF = 123456;
+const CPF = 123456
 console.log(CPF);
 
-let nome = "Gelado";
+let nome = "Gelado"
 console.log(nome);
+
+nome = "Ermeson"
+console.log(nome);
+
+var telefone = 654321
+console.log(telefone);
+var telefone = 123456
+console.log(telefone);
+
+let email_aluno = "geladinho.barros@percorre.org.br"
+let emailAluno = "geladinho.barros@percorre.org.br"
+let EMAILALUNO = "geladinho.barros@percorre.org.br"
+let EmailAluno = "geladinho.barros@percorre.org.br"
