4e9d1521843fcf560f5ff262a1ff7696d72eac74
Carlos <cadubalbueno2007@gmail.com>
2026-05-12 10:11:04 -0300

    Primeiro commit de js

---
+++ AULA01/main.js
@@ -0,0 +1,10 @@
+function soma() {
+    let primeiroNumero = parseFloat(prompt('Digite o primeiro número'));
+    let segundoNumero = parseFloat(prompt('Digite o segundo número'));
+    let resultado = primeiroNumero + segundoNumero;
+    return resultado
+}
+
+let somado = soma()
+
+alert(somado)
---
+++ AULA01/tipos_dados.js
@@ -0,0 +1,17 @@
+// "Ermeson" | 'Ermeson' | `Ermeson` = string
+// 7 | 7.0 = number
+// Verdadeiro | Falso = booleano
+
+console.log(`Ermeson Barros`);
+console.log(`Nota do aluno foi: ` + 8);
+console.log(6 >= 7);
+
+// const
+// let
+// var
+
+const CPF = 123456;
+console.log(CPF);
+
+let nome = "Gelado";
+console.log(nome);
